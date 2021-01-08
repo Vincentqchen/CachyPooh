8160679f229b347c479611aa151e3b6f99248d16
vincent chen <vincentqchen@outlook.com>
2021-01-08 00:53:22 -0500

    Finally fixed the video models

--- CacheDatabase/catalog/models.py
+++ CacheDatabase/catalog/models.py
@@ -7,7 +7,7 @@ from googleapiclient.discovery import build
 #Scheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
-class Video(models.Model):
+class YTVideo(models.Model):
     #youtube_cache = models.ForeignKey(YoutubeCache,on_delete=models.CASCADE)
     url = models.CharField(max_length=600, null=False, blank=False)
     title = models.CharField(max_length=600, null=False, blank=False)
@@ -15,40 +15,41 @@ class Video(models.Model):
     views = models.IntegerField(null=False, blank=False)
     date = models.DateField(auto_now=False, auto_now_add=False)
     ytType = models.CharField(max_length=30, null=False, blank=False)
-# # Create your models here.
-# def ytCacheHelper():
-#     youtube = build('youtube','v3',developerKey = 'AIzaSyBVb4q7yTC3-Gx7qAWIvPaeZUnJuUc6HkU')
+# Create your models here.
+def ytCacheHelper():
+    youtube = build('youtube','v3',developerKey = 'AIzaSyBVb4q7yTC3-Gx7qAWIvPaeZUnJuUc6HkU')
 
-#     #Most popular videos in the us
-#     YTrequest = youtube.videos().list(
-#         part="snippet,contentDetails,statistics",
-#         chart="mostPopular",
-#         regionCode="US"
-#     )
-#     response = YTrequest.execute()
-#     #Python dictionary of all top yt videos
-#     items = response['items']
-#     for x in range(len(items)):
-#         # Parts of video
-#         snippet = items[x]['snippet']
-#         contentDetails = items[x]['contentDetails']
-#         statistics = items[x]['statistics']
-#         # Figure out length of video
-#         length = contentDetails['duration']
-#         length = length[2:]
-#         #Figure out date of video
-#         date = snippet['publishedAt']
-#         year = int(date[:4])
-#         month = int(date[5:7])
-#         day = int(date[8:10])
-#         hour = int(date[11:13])
-#         minute = int(date[14:16])
-#         date = datetime(year, month, day, hour, minute)
-#         # Set up the video model
-#         mostViewedVideo = Video(url='https://www.youtube.com/watch?v='+items[x]['id'], title=snippet['title'], length=length, views=statistics['viewCount'], date=date, ytType='mostPopular')
-#         mostViewedVideo.save()
-
-# #Schedule daily database updates
+    #Most popular videos in the us
+    YTrequest = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        chart="mostPopular",
+        regionCode="US"
+    )
+    response = YTrequest.execute()
+    #Python dictionary of all top yt videos
+    items = response['items']
+    for x in range(len(items)):
+        # Parts of video
+        snippet = items[x]['snippet']
+        contentDetails = items[x]['contentDetails']
+        statistics = items[x]['statistics']
+        # Figure out length of video
+        length = contentDetails['duration']
+        length = length[2:]
+        #Figure out date of video
+        date = snippet['publishedAt']
+        year = int(date[:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+        hour = int(date[11:13])
+        minute = int(date[14:16])
+        date = datetime(year, month, day, hour, minute)
+        # Set up the video model
+        mostViewedVideo = YTVideo(url='https://www.youtube.com/watch?v='+items[x]['id'], title=snippet['title'], length=length, views=statistics['viewCount'], date=date, ytType='mostPopular')
+        mostViewedVideo.save()
+ytCacheHelper()
+print(YTVideo.objects.all())
+#Schedule daily database updates
 # scheduler = BackgroundScheduler()
 # scheduler.add_job(ytCacheHelper, 'interval', seconds=3)
 # scheduler.start()
