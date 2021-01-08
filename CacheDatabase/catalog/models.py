89a6ac996d662af445b6bcf5b562b8802bb598ce
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2021-01-08 09:15:58 +0530

    Dummy *homepage*

--- CacheDatabase/catalog/models.py
+++ CacheDatabase/catalog/models.py
@@ -23,43 +23,43 @@ class Video(models.Model):
 
 # Create your models here.
 
-def ytCacheHelper():
-    youtube = build('youtube','v3',developerKey = 'AIzaSyBVb4q7yTC3-Gx7qAWIvPaeZUnJuUc6HkU')
+# def ytCacheHelper():
+#     youtube = build('youtube','v3',developerKey = 'AIzaSyBVb4q7yTC3-Gx7qAWIvPaeZUnJuUc6HkU')
 
-    #Most popular videos in the us
-    YTrequest = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        chart="mostPopular",
-        regionCode="US"
-    )
-    response = YTrequest.execute()
-    #Python dictionary of all top yt videos
-    items = response['items']
-    for x in range(len(items)):
-        # Parts of video
-        snippet = items[x]['snippet']
-        contentDetails = items[x]['contentDetails']
-        statistics = items[x]['statistics']
-        print(snippet['title'])
+#     #Most popular videos in the us
+#     YTrequest = youtube.videos().list(
+#         part="snippet,contentDetails,statistics",
+#         chart="mostPopular",
+#         regionCode="US"
+#     )
+#     response = YTrequest.execute()
+#     #Python dictionary of all top yt videos
+#     items = response['items']
+#     for x in range(len(items)):
+#         # Parts of video
+#         snippet = items[x]['snippet']
+#         contentDetails = items[x]['contentDetails']
+#         statistics = items[x]['statistics']
+#         print(snippet['title'])
 
-        # Figure out length of video
-        length = contentDetails['duration']
-        length = length[2:]
-        #Figure out date of video
-        date = snippet['publishedAt']
-        year = int(date[:4])
-        month = int(date[5:7])
-        day = int(date[8:10])
-        hour = int(date[11:13])
-        minute = int(date[14:16])
-        date = datetime(year, month, day, hour, minute)
-        #date = datetime.fromisoformat(date)
-        # Set up the video model
-        mostViewedVideo = Video(url='https://www.youtube.com/watch?v='+items[x]['id'], title=snippet['title'], length=length, views=statistics['viewCount'], date=date, ytType='mostPopular')
-        mostViewedVideo.save()
+#         # Figure out length of video
+#         length = contentDetails['duration']
+#         length = length[2:]
+#         #Figure out date of video
+#         date = snippet['publishedAt']
+#         year = int(date[:4])
+#         month = int(date[5:7])
+#         day = int(date[8:10])
+#         hour = int(date[11:13])
+#         minute = int(date[14:16])
+#         date = datetime(year, month, day, hour, minute)
+#         #date = datetime.fromisoformat(date)
+#         # Set up the video model
+#         mostViewedVideo = Video(url='https://www.youtube.com/watch?v='+items[x]['id'], title=snippet['title'], length=length, views=statistics['viewCount'], date=date, ytType='mostPopular')
+#         mostViewedVideo.save()
     
 
-ytCacheHelper()
+# ytCacheHelper()
 #Schedule daily database updates
 # scheduler = BackgroundScheduler()
 # scheduler.add_job(ytCacheHelper, 'interval', seconds=3)
